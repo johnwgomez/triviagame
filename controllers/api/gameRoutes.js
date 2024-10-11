f0901b7625f4672282b32c1838bb902b070b5d8d
Antonio Kodheli <antoniokodheli@mac.myfiosgateway.com>
2024-10-10 21:44:52 -0400

    added leaderboard layout

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -8,15 +8,10 @@ const {triviaForm}= require('../../utils/opentdb')
 router.post('/game',async(req,res)=>{
     try{
         const {category, difficulty} = req.body;
-
-        console.log(category)
-        console.log(difficulty)
-
         const trivia =  await triviaForm(category,difficulty)
-        console.log(trivia)
         const question = trivia[0].question
-        console.log(question)
         const decodedQuestion = decodeString(question)
+
         const triviaAnswers = trivia[0].incorrect_answers
         const correctAnswer = trivia[0].correct_answer
         const decodedCorrect = decodeString(correctAnswer)
@@ -25,7 +20,8 @@ router.post('/game',async(req,res)=>{
             question: decodedQuestion,
             incorrects: triviaAnswers,
             correct: decodedCorrect,
-            logged_in: req.session.logged_in
+            logged_in: req.session.logged_in,
+            difficulty
         }
     )
     }catch(err){
@@ -33,6 +29,42 @@ router.post('/game',async(req,res)=>{
         console.log(err)
     }
 } )
+
+
+router.post('/finish', async(req,res)=> {
+    try{
+        const {difficulty, incorrect, correct}= req.body;
+        const userScore = await Score.findOne({
+            where: {
+                player_id: req.session.user_id
+            }
+        })
+        if(difficulty==='easy' && correct){
+            await userScore.increment('score_number')
+        }else if(difficulty==='medium' && correct){
+            await userScore.increment('score_number', {by: 2})
+        }else if(difficulty==='hard' && correct){
+            await userScore.increment('score_number', {by: 3})
+        }else if(incorrect){
+            res.render('try-again')
+        }
+    
+    }catch(err){
+        res.status(500).json(err)
+        console.log(err)
+    }
+})
+
+router.get('/something', async(req,res)=> {
+    const user = await Score.findOne({
+        where: {
+            player_id: req.session.user_id
+        }
+    })
+    console.log(user)
+    res.json(user)
+})
+
 
 
 
